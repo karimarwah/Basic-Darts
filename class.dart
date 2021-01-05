49ab49c027c9de3ff1da47089ab215848ec1ee66
Karima Marwazia Shaliha <55788227+karimarwah@users.noreply.github.com>
2021-01-05 13:42:58 +0700

    Add files via upload

---
+++ class.dart
@@ -0,0 +1,29 @@
+class Animal {
+  String name;
+  int age;
+  double weight;
+
+  Animal(this.name, this.age, this.weight);
+
+  void eat() {
+    print('$name is eating.');
+    weight = weight + 0.2;
+  }
+
+  void sleep() {
+    print('$name is sleeping.');
+  }
+
+
+  void poop() {
+    print('$name is pooping.');
+    weight = weight - 0.1;
+  }
+}
+var dicodingCat = Animal('Gray', 2, 4.2);
+void main() {
+  var dicodingCat = Animal('Gray', 2, 4.2);
+  dicodingCat.eat();
+  dicodingCat.poop();
+  print(dicodingCat.weight);
+}
